037a7e0164201567ce9986cb6ca20836ae3079f7
Carlos Salame <carlosandressalame@hotmail.com>
2025-02-25 18:03:39 -0500

    fixed home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 import "./App.css";
-import PixelatedImage from "./components/PixelatedImage";
 import SpotifyApi from "./components/SpotifyApi";
 
 function App() {
